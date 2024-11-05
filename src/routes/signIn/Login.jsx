f6104f29e883bcde7b0c6d523ba6eb1ba4889122
abhijithp327 <87142203+abhijithp327@users.noreply.github.com>
2024-11-05 18:33:25 +0530

    added css

--- src/routes/signIn/Login.jsx
+++ src/routes/signIn/Login.jsx
@@ -1,10 +1,11 @@
 import { SignIn } from '@clerk/clerk-react';
 import React from 'react'
+import './login.css'
 
 const Login = () => {
     return (
         <div className='signInPage'>
-            <SignIn path="/sign-in" />
+            <SignIn path="/sign-in" signUpUrl='/sign-up' />
         </div>
     )
 }
